061e9d144ae3dc31acd09c798a42860c7b116077
natsuto-kun <natsuto391@gmail.com>
2022-09-20 13:49:22 +0900

    canvasの描画test

--- src/views/components/pages/Game/index.tsx
+++ src/views/components/pages/Game/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from "next";
 import { game } from "./style";
+import { Canvas } from "./canvas";
+
 // gameページ
 export const Game: NextPage = () => {
   return (
     <div css={game}>
       <p>game</p>
+      <Canvas></Canvas>
+      <p>test</p>
     </div>
   );
 };
