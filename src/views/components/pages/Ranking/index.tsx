268e29ff07ccc56bc4aa0c703f3524acc4f7b60c
shu20031026 <shu20031026@gmail.com>
2022-09-20 19:22:37 +0900

    add: firestoreへアイテムを追加できるよう実装

--- src/views/components/pages/Ranking/index.tsx
+++ src/views/components/pages/Ranking/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next'
 import { useEffect, useState } from 'react'
 import { ranking } from './style'
 // rankingページ
-import { fetchData } from '@/utils/firestore'
+import { fetchData, putData } from '@/utils/firestore'
 
 export const Ranking: NextPage = () => {
   const [result, setResult] = useState<DocumentData[]>([])
@@ -24,6 +24,9 @@ export const Ranking: NextPage = () => {
           <li key={i}>{doc.unit}</li>
         ))}
       </ul>
+      <div onClick={() => putData({ name: 'shu nakashima', unit: ['フランス語'], droppedUnit: 1 })}>
+        button
+      </div>
     </div>
   )
 }
