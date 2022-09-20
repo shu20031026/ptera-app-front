0dbefa8822ddf5617fb19edc161c47604ea47a32
shu20031026 <shu20031026@gmail.com>
2022-09-20 18:43:30 +0900

    fix: インデントの修正

--- src/views/components/pages/Ranking/index.tsx
+++ src/views/components/pages/Ranking/index.tsx
@@ -1,37 +1,40 @@
-import type { NextPage } from "next";
-import { ranking } from "./style";
+import { collection, getDocs } from '@firebase/firestore'
+import type { DocumentData } from '@firebase/firestore'
+import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
+import { ranking } from './style'
 // rankingページ
-import { collection, getDocs, DocumentData } from "firebase/firestore";
-import { useEffect, useState } from "react";
-import { db } from "@/utils/firebase";
+import { db } from '@/constant/firebase'
 
 const fetchData = async () => {
-  const querySnapshot = await getDocs(collection(db, "koji-ranjer"))
+  const querySnapshot = await getDocs(collection(db, 'koji-ranjer'))
   const result: DocumentData[] = []
   querySnapshot.forEach((doc) => {
-    console.log(doc.id, " => ", doc.data());
+    console.log(doc.id, ' => ', doc.data())
     result.push(doc.data())
-  });
+  })
   return result
 }
 
 export const Ranking: NextPage = () => {
-  const [result,setResult] = useState<DocumentData[]>([])
+  const [result, setResult] = useState<DocumentData[]>([])
 
   useEffect(() => {
-    (async ()=>{
-      const data = await fetchData();
+    ;(async () => {
+      const data = await fetchData()
       console.debug(data)
       setResult(data)
     })()
-  },[])
+  }, [])
 
   return (
     <div css={ranking}>
       <p>ranking</p>
       <ul>
-        {result.map((doc, i) => <li key={i}>{doc.unit}</li>)}
+        {result.map((doc, i) => (
+          <li key={i}>{doc.unit}</li>
+        ))}
       </ul>
     </div>
-  );
-};
+  )
+}
