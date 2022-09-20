cbe6ffe5dc0a13726ff8e7aad9a7b8f6645d8a0e
shu20031026 <shu20031026@gmail.com>
2022-09-20 21:19:26 +0900

    fix: 不要なファイルの削除

--- src/pages/api/testdata/ranking.ts
+++ src/pages/api/testdata/ranking.ts
@@ -1,37 +1,30 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-const { initializeApp } = require('firebase-admin/app');
-const { cert } = require('firebase-admin/app');
-const { getFirestore } = require('firebase-admin/firestore');
-import * as admin from 'firebase-admin';
-import serviceAccount from 'ptera-cup-firebase-adminsdk-nscv8-b5fffc69d3.json';
-import { database } from "firebase-admin";
+import type { NextApiRequest, NextApiResponse } from 'next'
+const { initializeApp } = require('firebase-admin/app')
+const { cert } = require('firebase-admin/app')
+const { getFirestore } = require('firebase-admin/firestore')
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  
-  const db = getFirestore();
-  
-  if (req.method === 'GET'){
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const db = getFirestore()
+
+  if (req.method === 'GET') {
     const dummyRanking: any = [
       {
         name: 'よーくん',
         score: 100,
         droppedUnit: 1,
         unit: ['微積'],
-        id: 1
+        id: 1,
       },
       {
         name: 'よーくん',
         score: 50,
         droppedUnit: 2,
-        unit: ['微積','物理'],
-        id: 2
-      }
-    ];
+        unit: ['微積', '物理'],
+        id: 2,
+      },
+    ]
 
-    res.status(200).json(dummyRanking);
+    res.status(200).json(dummyRanking)
   }
-  res.status(200);
+  res.status(200)
 }
