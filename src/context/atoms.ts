0ec05d0dfc6a4a7cd88ccddc0d90631a4902b481
shu20031026 <shu20031026@gmail.com>
2022-09-21 22:54:41 +0900

    add: resultのデータのatomを作成

--- src/context/atoms.ts
+++ src/context/atoms.ts
@@ -5,10 +5,25 @@ export type UserDataType = {
   unitList: string[]
 }
 
+export type ResultType = {
+  userName: string
+  breakUnit: string[]
+  time: number
+}
+
 export const userDataState = atom<UserDataType>({
   key: 'userDataState',
   default: {
-    userName: '',
+    userName: 'unknown',
     unitList: [],
+  },
+})
+
+export const resultDataState = atom<ResultType>({
+  key: 'resultDataState',
+  default: {
+    userName: 'unKnown',
+    breakUnit: [],
+    time: 0,
   },
 })
--- src/views/components/pages/Result/index.tsx
+++ src/views/components/pages/Result/index.tsx
@@ -1,10 +1,26 @@
 import { NextPage } from 'next'
+import { useRecoilValue } from 'recoil'
 import { result } from './style'
+import { resultDataState } from '@/context/atoms'
 
 export const Result: NextPage = () => {
+  const resultData = useRecoilValue(resultDataState)
   return (
     <div css={result}>
       <div>result</div>
+      <div>
+        <p>{resultData.userName}</p>
+        <p>{resultData.time}</p>
+        <p>
+          {resultData.breakUnit.map((unit, index) => {
+            return (
+              <div key={index}>
+                <p>{unit}</p>
+              </div>
+            )
+          })}
+        </p>
+      </div>
     </div>
   )
 }
